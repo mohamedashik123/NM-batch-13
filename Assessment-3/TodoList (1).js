47e33e77b407a2d0d4cc02cbcf9902580440848a
mohamedashik123 <148215245+mohamedashik123@users.noreply.github.com>
2023-11-13 22:23:36 +0530

    Add files via upload

---
+++ Assessment-3/TodoList (1).js
@@ -0,0 +1,79 @@
+import React, {useEffect, useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+
+function TodoList() {
+
+  
+ 
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    
+    if (savedTodos) {
+      
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
+   const [todo, setTodo] = useState("");
+   useEffect(() => {
+    // localstorage only support storing strings as keys and values
+    // - therefore we cannot store arrays and objects without converting the object
+    // into a string first. JSON.stringify will convert the object into a JSON string
+    localStorage.setItem("todos", JSON.stringify(todos));
+    // add the todos as a dependancy because we want to update
+    // localstorage anytime the todos state changes
+  }, [todos]);
+
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
+}
+
+export default TodoList;
